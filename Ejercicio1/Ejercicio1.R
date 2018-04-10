45bfcb6f1631a45b49ef07d03cfa34936dc64990
LuisERG <lerg.10897@gmail.com>
2018-04-10 17:40:55 -0500

    Practica1

---
+++ Ejercicio1/Ejercicio1.R
@@ -0,0 +1,15 @@
+x<-seq(5, -1, by = -0.3)     # matches 'end'
+x<-rev(x)
+x
+
+y <- c(-1,3, -5,7, -9)
+y <- rep(y, each=10, times=2)
+y<-sort(y, decreasing=TRUE)
+y
+
+z<-(6:12)
+z<-c(z,rep(5.3,each=3))
+z<-c(z,-3)
+z<-c(z,rep(102:length(y), times=3))
+z
+length(z)
